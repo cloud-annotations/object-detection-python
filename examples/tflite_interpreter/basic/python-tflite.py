3a77ba3681d878cf754125c00cac01694d0561b1
mdunlop <matthewdunlop22@gmail.com>
2019-08-11 15:37:32 +0100

    Added more documentation to python-tflite.py

--- examples/tflite_interpreter/basic/python-tflite.py
+++ examples/tflite_interpreter/basic/python-tflite.py
@@ -6,10 +6,43 @@
 # https://www.linkedin.com/in/mdunlop2/
 
 '''
-Usage:
-cd to parent directory ../../basic
-python python-tflite.py --MODEL_DIR
-                        --
+## Tflite Object Detection
+Currently, `python-tflite.py` supports using Mobilenet-V1 SSD models trained using Cloud Annotations.
+
+Note: to find a list of all models trained do:
+```
+cacli list
+```
+
+To use a custom model, perform
+```
+cacli download <model_name>
+```
+For example, if the downloaded files were saved to `/path/to/<model_name>` :
+* Our tflite model is stored in `<model_name>/model_android/model.tflite`
+* Our tflite anchors file is stored in `<model_name>/model_android/anchors.json`
+* Our tflite labels file is stored in `<model_name>/model_android/labels.json`
+
+Change directory to the root of this git.
+```
+cd examples/tflite_interpreter/basic/
+python python-tflite.py --MODEL_DIR /path/to/<model_name>/model_android
+```
+This script calls the tflite model interpreter for inference on all .jpg files inside the directory `PATH_TO_TEST_IMAGES_DIR`.
+
+Similary the output .jpg files are storesd in `PATH_TO_OUTPUT_DIR`.
+
+We can also specify the minimum confidence (score) for a given detection box to be displayed with `MINIMUM_CONFIDENCE`.
+
+Finally:
+```
+python python-tflite.py \
+--MODEL_DIR /path/to/<model_name>/model_android \
+--PATH_TO_TEST_IMAGES_DIR /path/to/test/images \
+--PATH_TO_OUTPUT_DIR /path/to/output/images \
+--MINIMUM_CONFIDENCE 0.01
+
+```
 '''
 import glob
 import os
